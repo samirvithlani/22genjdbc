3d689683c555cb2b77445111b06bc07e5eefcaef
samirvithlani <samir.vithlani83955@gmail.com>
2023-09-17 13:54:09 +0530

    ok

--- src/com/dao/EmployeeDao.java
+++ src/com/dao/EmployeeDao.java
@@ -3,9 +3,28 @@ package com.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.util.DBConnection;
+
+class Employee {
+
+	int eId;
+	String eName;
+	int eAge;
+	String eEmail;
+
+	public Employee(int eId, String eName, int eAge, String eEmail) {
+
+		this.eId = eId;
+		this.eName = eName;
+		this.eAge = eAge;
+		this.eEmail = eEmail;
+	}
+
+}
 
 public class EmployeeDao {
 
@@ -52,10 +71,94 @@ public class EmployeeDao {
 				pstmt.setString(2, "amit@gmail.com");
 				pstmt.setInt(3, 34);
 				pstmt.setInt(4, 2);
-				
+
 				int res = pstmt.executeUpdate();
 				System.out.println(res);
-				
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public void addEmployees() {
+
+		Connection conn = DBConnection.getDbConnection();
+		if (conn != null) {
+
+			String insertSQL = "insert into employee(ename,eemail,eage)values(?,?,?)";
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(insertSQL);
+
+				pstmt.setString(1, "ram");
+				pstmt.setString(2, "ram1@gmail.com");
+				pstmt.setInt(3, 24);
+
+				pstmt.addBatch();
+
+				pstmt.setString(1, "sita");
+				pstmt.setString(2, "sita@gmail.com");
+				pstmt.setInt(3, 23);
+
+				pstmt.addBatch();
+				pstmt.setString(1, "kush");
+				pstmt.setString(2, "kush@gmail.com");
+				pstmt.setInt(3, 3);
+
+				pstmt.addBatch();
+
+				// batch execute..
+
+				int res[] = pstmt.executeBatch();
+				if (res.length > 0) {
+
+					System.out.println(res.length + " employee inseted...");
+				} else {
+
+					System.out.println("No employee inserted...");
+				}
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public void addEmpList() {
+
+		List<Employee> emplist = new ArrayList<Employee>();
+		emplist.add(new Employee(101, "rahul", 23, "rahul@gmail.com"));
+		emplist.add(new Employee(102, "priya", 26, "priya@gmail.com"));
+
+		Connection conn = DBConnection.getDbConnection();
+		String insertSQL = "insert into employee(ename,eemail,eage)values(?,?,?)";
+		if (conn != null) {
+
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(insertSQL);
+				for (Employee e : emplist) {
+
+					pstmt.setString(1, e.eName);
+					pstmt.setString(2, e.eEmail);
+					pstmt.setInt(3, e.eAge);
+					pstmt.addBatch();
+				}
+
+				int res[] = pstmt.executeBatch();
+				if (res.length > 0) {
+
+					System.out.println(res.length + " employee inseted...");
+				} else {
+
+					System.out.println("No employee inserted...");
+				}
+
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -68,8 +171,10 @@ public class EmployeeDao {
 	public static void main(String[] args) {
 
 		EmployeeDao employeeDao = new EmployeeDao();
-		//employeeDao.addEmployee();
-		employeeDao.updateEmployee();
+		// employeeDao.addEmployee();
+		// employeeDao.updateEmployee();
+		//employeeDao.addEmployees();
+		employeeDao.addEmpList();
 
 	}
 }
